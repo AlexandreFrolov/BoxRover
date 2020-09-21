7f2bc7d16cfa94fe067eede698d5032ba9f9ff5c
Александр Фролов <frolov@itmatrix.ru>
2020-09-21 14:29:16 +0300

    Update e32-get-config.py

--- ch17/py/e32-get-config.py
+++ ch17/py/e32-get-config.py
@@ -7,7 +7,6 @@ import time
 import sys
 from time import sleep
 
-print("It's UART test")
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -15,7 +14,6 @@ M0 = 22
 M1 = 27
 CFG_CMD = [b'\xC1\xC1\xC1',
 		   b'\xC3\xC3\xC3']
-
 
 GPIO.setup(M0,GPIO.OUT)
 GPIO.setup(M1,GPIO.OUT)
@@ -25,7 +23,6 @@ GPIO.setup(M1,GPIO.OUT)
 
 GPIO.output(M0,GPIO.HIGH)
 GPIO.output(M1,GPIO.HIGH)
-
 time.sleep(1)
 
 #ser = serial.Serial("/dev/ttyS0",9600)
@@ -33,23 +30,16 @@ ser = serial.Serial("/dev/serial0", 9600)
 ser.flushInput()
 
 try :
-	if ser.isOpen() :
-            print("It's setting RELAY mode")
-            ser.write("This is a GET CONFIG cmd:\r\n".encode())
-            ser.write(CFG_CMD[0])
-            ser.write("Received.\r\n".encode())
+    if ser.isOpen() :
+        ser.write(CFG_CMD[0])
 
 except :
-	if ser.isOpen() :
-		ser.close()
+    if ser.isOpen() :
+        ser.close()
 	GPIO.cleanup()
 
-
-while True:
-    received_data = ser.read()              #read serial port
-    sleep(0.03)
-    data_left = ser.inWaiting()             #check for remaining byte
-    received_data += ser.read(data_left)
-    print (received_data)                   #print received data
-    ser.write(received_data)                #transmit data serially
-
+received_data = ser.read(6)
+sleep(0.03)
+#data_left = ser.inWaiting()
+#received_data += ser.read(data_left)
+print('E32 config: {}'.format(received_data.encode('hex')))
